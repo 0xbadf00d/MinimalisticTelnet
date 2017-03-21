249b808792dfb2fff9ceb24683a9fa4fabe84bd4
Aaron Salisbury <aaron.salisbury@live.com>
2017-03-21 00:29:47 -0500

    Recreate project to target .NET Standard and other miscellaneous refactoring.

--- src/MinimalisticTelnet/TelnetInterface.cs
+++ src/MinimalisticTelnet/TelnetInterface.cs
@@ -1,58 +1,78 @@
-// minimalistic telnet implementation
-// conceived by Tom Janssens on 2007/06/06 for codeproject
+﻿// minimalistic telnet implementation
+// conceived by Tom Janssens on 2007/06/06  for codeproject
 //
 // http://www.corebvba.be
 
 // Modifications
 //
-// Date Person Description
-// ========== ========= ==============================================================================
-// 2013-06-06 jsagara   Implements IDisposable. Miscellaneous refactoring.
-// 2014-05-15 mewiii    Implements new methods for working with HP switches;
-//                      Implements setter/getter methods to change the default timeout value;
-//                      Implements new methods for reading until some text you're looking for is found
-//                      Implements Close() method to disconnect from remote host
-// 2015-02-05 0xbadf00d Implements prompt variable and uses it with ReadUntilString to capture returned
-//                      data using the Command() method.
-//                      Implements Enable() & isEnabled() to enable Cisco devices and check status.
-//                      Implements char[] _toTrim as null characters seen on some Cisco Nexus devices
+//Date       Person          Description
+//========== =============== ============================================================================================================================================
+//2007-06-06 Tom Janssens    Shared at https://www.codeproject.com/articles/19071/quick-tool-a-minimalistic-telnet-library
+//2013-06-06 jsagara         Implemented IDisposable and other miscellaneous refactoring.
+//                               Introduced project to GitHub at https://github.com/jonsagara/MinimalisticTelnet
+//2014-05-15 mewiii          Implemented new methods for working with HP switches:
+//						         Implements setter/getter methods to change the default timeout value.
+//                               Implements new methods for reading until some text you're looking for is found.
+//							     Implements Close() method to disconnect from remote host.
+//2015-02-05 0xbadf00d       Implements prompt variable and uses it with ReadUntilString to capture returned data using the Command() method.
+//                               Implements Enable() & isEnabled() to enable Cisco devices and check status.
+//                               Implements char[] _toTrim as null characters seen on some Cisco Nexus devices.
+//2017-03-21 Aaron Salisbury Recreate project to target .NET Standard and other miscellaneous refactoring.
 
 using System;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 namespace MinimalisticTelnet
 {
     public class TelnetConnection : IDisposable
     {
-        private TcpClient tcpSocket;
-        private int _TimeoutMs = 100;
-        private int _ReadUntilLimit = 3;
-        private string _prompt = "";
-        private char[] _toTrim = { '\n', '\r', '\0' };
+        private static readonly char[] TO_TRIM = { '\n', '\r', '\0' };
+
+        private enum Verbs { Will = 251, Wont, Do, Dont, Iac }
+        private enum Options { Sga = 3 }
+
+        private TcpClient _tcpSocket;
+        public TcpClient TcpSocket
+        {
+            set { _tcpSocket = value; }
+            get { return _tcpSocket; }
+        }
+
+        private int _timeoutMs;
+        public int TimeoutMs
+        {
+            set { _timeoutMs = value; }
+            get { return _timeoutMs; }
+        }
+
+        private int _readUntilLimit;
+        public int ReadUntilLimit
+        {
+            set { _readUntilLimit = value; }
+            get { return _readUntilLimit; }
+        }
+
+        private string _prompt;
+        public string Prompt
+        {
+            set { _prompt = value; }
+            get { return _prompt; }
+        }
 
         public bool IsConnected
         {
-            get { return tcpSocket.Connected; }
-        }
-
-        public int TimeoutMs
-        {
-            set { this._TimeoutMs = value; }
-            get { return _TimeoutMs; }
-        }
-
-        public int ReadUntilLimit
-        {
-            set { this._ReadUntilLimit = value; }           
-            get { return _ReadUntilLimit; }
+            get { return TcpSocket.Connected; }
         }
 
         public TelnetConnection(string hostname, int port)
         {
-            tcpSocket = new TcpClient(hostname, port);
+            TimeoutMs = 100;
+            ReadUntilLimit = 3;
+            Prompt = string.Empty;
+            TcpSocket = new TcpClient();
+            TcpSocket.ConnectAsync(hostname, port);
         }
 
         ~TelnetConnection()
@@ -60,43 +80,146 @@ namespace MinimalisticTelnet
             Dispose(false);
         }
 
-        // prompt set by Login when last line is trimmed using char[] _toTrim
-        public string Login(string username, string password, int loginTimeoutMs)
+        /// <summary>
+        /// Parses the server output after the initial connection. It will look for a colon ":" in the screen output, and will send username and password after the colon.
+        /// </summary>
+        /// <param name="regexMatchPattern">To check if the connection succeeded, server output ending with a "$" or a ">" is assumed.</param>
+        /// <returns></returns>
+        public string Login(string username, string password, int loginTimeoutMs, string regexMatchPattern = "^.*$")
         {
             int oldTimeoutMs = TimeoutMs;
             TimeoutMs = loginTimeoutMs;
 
-            string s = Read();
-            if (!s.TrimEnd().EndsWith(":"))
+            string serverOutput = Read();
+            if (!serverOutput.TrimEnd().EndsWith(":"))
             {
                 throw new Exception("Failed to connect : no login prompt");
             }
 
             WriteLine(username);
 
-            s += Read();
-            if (!s.TrimEnd().EndsWith(":"))
+            serverOutput += Read();
+            if (!serverOutput.TrimEnd().EndsWith(":"))
             {
                 throw new Exception("Failed to connect : no password prompt");
             }
 
             WriteLine(password);
 
-            s += Read();
+            serverOutput += Read();
 
             TimeoutMs = oldTimeoutMs;
 
-            Match match = Regex.Match(s, "^.*$", RegexOptions.Multiline | RegexOptions.RightToLeft);
+            Match match = Regex.Match(serverOutput, regexMatchPattern, RegexOptions.Multiline | RegexOptions.RightToLeft);
             if (match.Success)
             {
-                _prompt = match.Value.Trim(_toTrim);
+                Prompt = match.Value.Trim(TO_TRIM);
             }
 
-            return s;
+            return serverOutput;
         }
 
-        // added this method useful with Cisco routers that ask for a password only
-        // based on Login() above
+        public void WriteLine(string cmd)
+        {
+            Write(cmd + "\n");
+        }
+
+        public void Write(string cmd)
+        {
+            if (!TcpSocket.Connected)
+            {
+                return;
+            }
+
+            byte[] buf = Encoding.ASCII.GetBytes(cmd.Replace("\0xFF", "\0xFF\0xFF"));
+            TcpSocket.GetStream().Write(buf, 0, buf.Length);
+        }
+
+        /// <summary>
+        /// Assumes that if there is no data available for more then TimeOutMs milliseconds, the output is complete.
+        /// </summary>
+        /// <returns></returns>
+        public string Read()
+        {
+            if (!TcpSocket.Connected)
+            {
+                return null;
+            }
+
+            var sb = new StringBuilder();
+            do
+            {
+                ParseTelnet(sb);
+                System.Threading.Tasks.Task.Delay(TimeoutMs).Wait();
+
+            } while (TcpSocket.Available > 0);
+
+            return sb.ToString();
+        }
+
+        private void ParseTelnet(StringBuilder sb)
+        {
+            while (TcpSocket.Available > 0)
+            {
+                int input = TcpSocket.GetStream().ReadByte();
+                switch (input)
+                {
+                    case -1:
+                        break;
+
+                    case (int)Verbs.Iac:
+                        // interpret as command
+                        int inputVerb = TcpSocket.GetStream().ReadByte();
+                        if (inputVerb == -1)
+                        {
+                            break;
+                        }
+
+                        switch (inputVerb)
+                        {
+                            case (int)Verbs.Iac:
+                                // literal IAC = 255 escaped, so append char 255 to string
+                                sb.Append(inputVerb);
+                                break;
+
+                            case (int)Verbs.Do:
+                            case (int)Verbs.Dont:
+                            case (int)Verbs.Will:
+                            case (int)Verbs.Wont:
+                                // reply to all commands with "WONT", unless it is SGA (suppres go ahead)
+                                int inputoption = TcpSocket.GetStream().ReadByte();
+                                if (inputoption == -1)
+                                {
+                                    break;
+                                }
+
+                                TcpSocket.GetStream().WriteByte((byte)Verbs.Iac);
+
+                                if (inputoption == (int)Options.Sga)
+                                {
+                                    TcpSocket.GetStream().WriteByte(inputVerb == (int)Verbs.Do ? (byte)Verbs.Will : (byte)Verbs.Do);
+                                }
+                                else
+                                {
+                                    TcpSocket.GetStream().WriteByte(inputVerb == (int)Verbs.Do ? (byte)Verbs.Wont : (byte)Verbs.Dont);
+                                }
+
+                                TcpSocket.GetStream().WriteByte((byte)inputoption);
+                                break;
+                        }
+
+                        break;
+
+                    default:
+                        sb.Append((char)input);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Useful with Cisco routers that ask for a password only.
+        /// </summary>
         public string PasswordOnlyLogin(string Password, int LoginTimeoutMs)
         {
             int oldTimeoutMs = TimeoutMs;
@@ -104,18 +227,21 @@ namespace MinimalisticTelnet
             string s = Read();
 
             if (!s.TrimEnd().EndsWith(":"))
+            {
                 throw new Exception("Failed to connect : no password prompt");
+            }
+
             WriteLine(Password);
 
             s += Read();
             TimeoutMs = oldTimeoutMs;
+
             return s;
         }
 
-        // added this generic method useful with HP Switches that require that a space
-        // be sent prior to entering the password
-        // the user name is optional when it is left empty
-        // HP switches output lots of escape sequences and it is unlikely a prompt cleanly ends with #, $, >, etc.
+        /// <summary>
+        /// Helpful for switches that require that a space be sent prior to entering the password. HP switches output lots of escape sequences and it is unlikely a prompt cleanly ends with #, $, >, etc.
+        /// </summary>
         public string HPGenericLogin(string Username, string Password, bool sendSpace, int LoginTimeoutMs)
         {
             int oldTimeoutMs = TimeoutMs;
@@ -138,6 +264,7 @@ namespace MinimalisticTelnet
             WriteLine(Password);
 
             TimeoutMs = oldTimeoutMs;
+
             return s;
         }
 
@@ -147,7 +274,7 @@ namespace MinimalisticTelnet
             string s = ReadUntilString("log out [y/n]?");
             WriteLine("y");
 
-            this.Close();
+            Close();
 
             return s;
         }
@@ -164,42 +291,9 @@ namespace MinimalisticTelnet
             return s;
         }
 
-        public void WriteLine(string cmd)
-        {
-            Write(cmd + "\n");
-        }
-
-        public void Write(string cmd)
-        {
-            if (!tcpSocket.Connected)
-            {
-                return;
-            }
-
-            byte[] buf = ASCIIEncoding.ASCII.GetBytes(cmd.Replace("\0xFF", "\0xFF\0xFF"));
-            tcpSocket.GetStream().Write(buf, 0, buf.Length);
-        }
-
-        public string Read()
-        {
-            if (!tcpSocket.Connected)
-            {
-                return null;
-            }
-
-            var sb = new StringBuilder();
-            do
-            {
-                ParseTelnet(sb);
-                Thread.Sleep(TimeoutMs);
-
-            } while (tcpSocket.Available > 0);
-
-            return sb.ToString();
-        }
-
-        // based on Read()
-        // read until find text is found in the input stream
+        /// <summary>
+        /// Read until find text is found in the input stream.
+        /// </summary>
         public string ReadUntilString(string findText)
         {
             int counter = ReadUntilLimit;
@@ -221,17 +315,19 @@ namespace MinimalisticTelnet
                 }
                 s += s2;
             }
+
             return s;
         }
 
-        //based on ReadUntilString(string findnext)
-        //using the prompt variable send the command and reads until the prompt is found.
+        /// <summary>
+        /// Using the prompt variable, sends the command and reads until the prompt is found.
+        /// </summary>
         public string Command(string command)
         {
             WriteLine(command);
             int counter = ReadUntilLimit;
             string s = Read();
-            while (!s.Contains(_prompt))
+            while (!s.Contains(Prompt))
             {
                 string s2 = Read();
                 if (s2.Length == 0)
@@ -248,11 +344,10 @@ namespace MinimalisticTelnet
                 }
                 s += s2;
             }
+
             return s;
-            
         }
 
-        // Enable method based on Login, prompt set after enable attempted
         public void Enable(string enablePassword, int EnableTimeoutMs)
         {
             int oldTimeoutMs = TimeoutMs;
@@ -262,7 +357,10 @@ namespace MinimalisticTelnet
             string s = Read();
 
             if (!s.TrimEnd().EndsWith(":"))
+            {
                 throw new Exception("Failed to enable : no password prompt");
+            }
+
             WriteLine(enablePassword);
 
             s += Read();
@@ -271,16 +369,18 @@ namespace MinimalisticTelnet
             Match match = Regex.Match(s, "^.*$", RegexOptions.Multiline | RegexOptions.RightToLeft);
             if (match.Success)
             {
-                _prompt = match.Value.Trim(_toTrim);
+                Prompt = match.Value.Trim(TO_TRIM);
             }
-            
         }
 
-        // Finds the last character of the current prompt returns true if it is '#'
-        // retruns false if it is '>' throws an Exception if it is niether
-        public bool isEnabled()
+        /// <summary>
+        /// Finds the last character of the current prompt.
+        /// </summary>
+        /// <returns>True if last character is equal to '#' and False if it is greater than that.</returns>
+        public bool IsEnabled()
         {
-            char last = _prompt[_prompt.Length - 1];
+            char last = Prompt[Prompt.Length - 1];
+
             if (last == '#')
             {
                 return true;
@@ -290,66 +390,8 @@ namespace MinimalisticTelnet
                 return false;
             }
             else
-                throw new Exception("Unusual prompt character found");
-        }
-
-        private void ParseTelnet(StringBuilder sb)
-        {
-            while (tcpSocket.Available > 0)
             {
-                int input = tcpSocket.GetStream().ReadByte();
-                switch (input)
-                {
-                    case -1:
-                        break;
-
-                    case (int)Verbs.Iac:
-                        // interpret as command
-                        int inputVerb = tcpSocket.GetStream().ReadByte();
-                        if (inputVerb == -1)
-                        {
-                            break;
-                        }
-
-                        switch (inputVerb)
-                        {
-                            case (int)Verbs.Iac:
-                                // literal IAC = 255 escaped, so append char 255 to string
-                                sb.Append(inputVerb);
-                                break;
-
-                            case (int)Verbs.Do:
-                            case (int)Verbs.Dont:
-                            case (int)Verbs.Will:
-                            case (int)Verbs.Wont:
-                                // reply to all commands with "WONT", unless it is SGA (suppres go ahead)
-                                int inputoption = tcpSocket.GetStream().ReadByte();
-                                if (inputoption == -1)
-                                {
-                                    break;
-                                }
-
-                                tcpSocket.GetStream().WriteByte((byte)Verbs.Iac);
-
-                                if (inputoption == (int)Options.Sga)
-                                {
-                                    tcpSocket.GetStream().WriteByte(inputVerb == (int)Verbs.Do ? (byte)Verbs.Will : (byte)Verbs.Do);
-                                }
-                                else
-                                {
-                                    tcpSocket.GetStream().WriteByte(inputVerb == (int)Verbs.Do ? (byte)Verbs.Wont : (byte)Verbs.Dont);
-                                }
-
-                                tcpSocket.GetStream().WriteByte((byte)inputoption);
-                                break;
-                        }
-
-                        break;
-
-                    default:
-                        sb.Append((char)input);
-                        break;
-                }
+                throw new Exception("Unusual prompt character found");
             }
         }
 
@@ -357,7 +399,8 @@ namespace MinimalisticTelnet
         {
             if (IsConnected)
             {
-                tcpSocket.Close();
+                TcpSocket.Dispose();
+                TcpSocket = new TcpClient();
             }
         }
 
@@ -371,31 +414,12 @@ namespace MinimalisticTelnet
         {
             if (disposing)
             {
-                if (tcpSocket != null)
+                if (TcpSocket != null)
                 {
-                    ((IDisposable)tcpSocket).Dispose();
-                    tcpSocket = null;
+                    ((IDisposable)TcpSocket).Dispose();
+                    TcpSocket = null;
                 }
             }
         }
-
-
-        #region Private Enums
-
-        enum Verbs
-        {
-            Will = 251,
-            Wont = 252,
-            Do = 253,
-            Dont = 254,
-            Iac = 255
-        }
-
-        enum Options
-        {
-            Sga = 3
-        }
-
-        #endregion
     }
 }
